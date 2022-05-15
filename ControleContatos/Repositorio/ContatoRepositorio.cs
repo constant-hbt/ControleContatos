079d5f46d81d5c5bed8e29f182cb1e85e5b72473
Henrique Borges <henrique17_votu@hotmail.com>
2022-05-14 23:21:07 -0300

    Adicionado Banco de dados, listagem e criação de contatos

---
+++ ControleContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using ControleContatos.Data;
+using ControleContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public List<ContatoModel> BuscarTodos()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+            return contato;
+        }
+
+        
+    }
+}
