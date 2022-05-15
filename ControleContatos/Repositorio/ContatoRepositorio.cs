4a2430876f8681a04594449b1de7479a5753fa93
Henrique Borges <henrique17_votu@hotmail.com>
2022-05-15 13:01:55 -0300

    Adição das funcionalidades de Editar e Apagar contato

--- ControleContatos/Repositorio/ContatoRepositorio.cs
+++ ControleContatos/Repositorio/ContatoRepositorio.cs
@@ -14,6 +14,11 @@ namespace ControleContatos.Repositorio
         {
             _bancoContext = bancoContext;
         }
+
+        public ContatoModel ListarPorId(int id)
+        {
+            return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+        }
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
@@ -26,6 +31,28 @@ namespace ControleContatos.Repositorio
             return contato;
         }
 
-        
+        ContatoModel IContatoRepositorio.Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDb = ListarPorId(contato.Id);
+            if (contatoDb == null) throw new Exception("Houve um erro na atualização do contato");
+
+            contatoDb.Nomne = contato.Nomne;
+            contatoDb.Email = contato.Email;
+            contatoDb.Celular = contato.Celular;
+
+            _bancoContext.Contatos.Update(contatoDb);
+            _bancoContext.SaveChanges();
+
+            return contatoDb;
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel contatoDb = ListarPorId(id);
+            if (contatoDb == null) throw new Exception("Houve uma deleção do contato");
+            _bancoContext.Contatos.Remove(contatoDb);
+            _bancoContext.SaveChanges();
+            return true;
+        }
     }
 }
